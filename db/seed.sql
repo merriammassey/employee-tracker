263b3d392646b047e1a78489cf40be0a7427888a
Maria M <prov21_1@hotmail.com>
2021-04-25 15:40:21 -0700

    edited seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,17 +1,17 @@
 INSERT INTO department(name)
 VALUES
-  ('devops'),
-  ('ux'),
-  ('hr');
+  ('DevOps'),
+  ('UX'),
+  ('HR');
 
 INSERT INTO role(title, salary, department_id)
 VALUES
-  ('director', 120000, 1),
-  ('architect', 150000, 2),
-  ('manager', 100000, 3);
+  ('Director', 120000, 1),
+  ('Architect', 150000, 2),
+  ('Manager', 100000, 3);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
-  ('merriam', 'massey', 1, 1),
-  ('matt', 'reines', 2, 2),
-  ('maribel', 'ellison', 3, 3);
+  ('Daniela', 'Ryf', 1, 1),
+  ('Lionel', 'Sanders', 2, 2),
+  ('Heather', 'Jackson', 3, 3);
